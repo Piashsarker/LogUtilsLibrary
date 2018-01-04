68a3d9066cdeccbc7d89e1f3c5a1730b851f2b04
Piash Sarker <sarkerpt@gmail.com>
2018-01-04 17:13:57 +0600

    added error log util

--- app/src/main/java/com/dcastalia/loginutilsexample/MainActivity.java
+++ app/src/main/java/com/dcastalia/loginutilsexample/MainActivity.java
@@ -3,11 +3,17 @@ package com.dcastalia.loginutilsexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dcastalia.logutil.LogDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogDebug logDebug = new LogDebug();
+        logDebug.d("MainActivity","Log From Main Activity");
+
+
     }
 }
---
+++ errorlogutil/src/main/java/com/dcastalia/errorlogutil/LogError.java
@@ -0,0 +1,13 @@
+package com.dcastalia.errorlogutil;
+
+import android.util.Log;
+
+/**
+ * Created by piashsarker on 1/4/18.
+ */
+
+public class LogError {
+    public static void e(String tag , String message){
+        Log.e(tag , message);
+    }
+}
