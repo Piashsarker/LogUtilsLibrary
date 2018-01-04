adf9bbf3319c502b6d2894028febca5d6d6a3a8c
Piash Sarker <sarkerpt@gmail.com>
2018-01-04 13:41:21 +0600

    Log Library Added

---
+++ logutil/src/main/java/com/dcastalia/logutil/LogDebug.java
@@ -0,0 +1,14 @@
+package com.dcastalia.logutil;
+
+import android.util.Log;
+
+/**
+ * Created by piashsarker on 1/4/18.
+ */
+
+public class LogDebug {
+
+    public void d(String tag , String message){
+        Log.d(tag , message);
+    }
+}
