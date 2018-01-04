8cbf1ec90ee15322d793428f8c0ba85346bfcdf2
Piash Sarker <sarkerpt@gmail.com>
2018-01-04 17:59:07 +0600

    test library work done

--- app/src/main/java/com/dcastalia/loginutilsexample/MainActivity.java
+++ app/src/main/java/com/dcastalia/loginutilsexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.dcastalia.loginutilsexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dcastalia.errorlogutil.LogError;
 import com.dcastalia.logutil.LogDebug;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LogDebug logDebug = new LogDebug();
         logDebug.d("MainActivity","Log From Main Activity");
-
+        LogError.e("MainActivity","Error Log From Main Activity");
 
     }
 }
